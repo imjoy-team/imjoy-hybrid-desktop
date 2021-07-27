fae22a95c7707835b96e65fad6d79fbb35124ad5
oeway <oeway007@gmail.com>
2021-07-27 19:11:27 +0200

    Use random port

--- xpra_server/xpra_server.py
+++ xpra_server/xpra_server.py
@@ -4,13 +4,12 @@ def setup_server():
     return {
         'command': [
             'xpra', 'start',
-            '--bind-tcp=0.0.0.0:5909',
+            '--bind-tcp=0.0.0.0:{port}',
             '--html=' + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'html5'),
             '--start=lxterminal',
             '--daemon=no',
             ":200"
         ],
-        'port': 5909,
         'timeout': 30,
         'launcher_entry': {
             "title": "Desktop",
