91f4ee58dabd1c5432ec16a17a3f3014166ab386
oeway <oeway007@gmail.com>
2021-07-30 00:04:36 +0200

    Upgrade xpra for the fix

--- xpra_server/html5/js/Window.js
+++ xpra_server/html5/js/Window.js
@@ -315,24 +315,28 @@ XpraWindow.prototype.ensure_visible = function() {
 	if (this.client.server_is_desktop || this.client.server_is_shadow) {
 		//those windows should usually be centered on screen,
 		//moving them would mess that up
-		return;
+		return true;
+	}
+	if (this.override_redirect) {
+		//OR windows cannot be moved server-side
+		return true;
 	}
 	const oldx = this.x;
 	const oldy = this.y;
 	// for now make sure we don't out of top left
 	// this will be much smarter!
-	const min_visible = 0;
+	const min_visible = 80;
 	const desktop_size = this.client._get_desktop_size();
 	const ww = desktop_size[0];
 	const wh = desktop_size[1];
 	//this.log("x=", this.x, "y=", this.y, "w=", this.w, "h=", this.h, "leftoffset=", this.leftoffset, "topoffset=", this.topoffset, " - ww=", ww, "wh=", wh);
-	if(oldx + this.w <= min_visible) {
+	if(oldx<this.leftoffset && oldx + this.w <= min_visible) {
 		this.x = min_visible - this.w + this.leftoffset;
 	}
 	else if (oldx >= ww - min_visible) {
 		this.x = Math.min(oldx, ww - min_visible);
 	}
-	if(oldy <= min_visible) {
+	if(oldy<=this.topoffset && oldy <= min_visible) {
 		this.y = parseInt(this.topoffset);
 	}
 	else if (oldy >= wh - min_visible) {
@@ -866,7 +870,9 @@ XpraWindow.prototype.screen_resized = function() {
 		this.fill_screen();
 		this.handle_resized();
 	}
-	this.ensure_visible();
+	if (!this.ensure_visible()) {
+		this.geometry_cb(this);
+	}
 };
 
 XpraWindow.prototype.recenter = function(force_update_geometry) {
@@ -960,7 +966,9 @@ XpraWindow.prototype.move_resize = function(x, y, w, h) {
 			// is this the right thing to do?
 			this.geometry_cb(this);
 		}
-		this.updateCSSGeometry();
+		else {
+			this.updateCSSGeometry();
+		}
 	}
 };
 
